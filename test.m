ca9b64069b3c4476a83f9c0ab93ba9ffa15db4ab
Torschden <132200003+Torschden@users.noreply.github.com>
2025-04-15 10:46:28 +0200

    Update test.m

--- test.m
+++ test.m
@@ -2,13 +2,17 @@ clear all
 close all 
 clc
 
-disp("calculating")
+disp("running")
 % a = 1;
 % B = 2;
 % save('data.mat',"a","B");
 data = load("data.mat");
+disp("read file")
 x = data.a + data.B;
+disp("calculating")
+disp("result: " + x)
 res = fopen("test.txt","wt");
 fprintf(res, '%d', x);
+disp("wrote resultfile")
 fclose(res);
 disp("done")
